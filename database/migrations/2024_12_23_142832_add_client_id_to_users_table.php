675282dcc64fc83e98360d2fcbb5235cd39ebcb0
RedenLamosa <redenlamosa@gmail.com>
2024-12-23 14:32:26 +0800

    Added Client ID on Users Table

---
+++ database/migrations/2024_12_23_142832_add_client_id_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddClientIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('client_id')->nullable()->after('is_verified');
+
+            $table->foreign('client_id')->references('id')->on('clients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('client_id');
+        });
+    }
+}
