6113ee730003952a284e2e78c94b32d940f75e0d
RedenLamosa <redenlamosa@gmail.com>
2024-12-26 11:29:43 +0800

    Clients  Controller Restrictions

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -13,57 +13,78 @@ use Illuminate\Support\Facades\Auth;
 
 class ClientsController extends Controller
 {
+    public function checkUser()
+    {
+        Log::info("ClientsController::checkUser");
+
+        if (Auth::check()) {
+            $user = Auth::user();
+
+            if ($user->user_type == 'SuperAdmin') {
+                return true;
+            }
+        }
+
+        return false;
+    }
     public function getClients(Request $request)
     {
-        log::info("ClientsController::getClients");
-    }
+        Log::info("ClientsController::getClients");
 
+        if ($this->checkUser()) {
+            $clients = ClientsModel::get();
+
+            return response()->json(['status' => 200, 'clients' => $clients]);
+        }
+
+        return response()->json(['status' => 200, 'clients' => null]);
+    }
     public function saveClient(Request $request)
     {
         log::info("ClientsController::saveClient");
 
-        try {
-            DB::beginTransaction();
+        if ($this->checkUser()) {
+            try {
+                DB::beginTransaction();
 
-            $client = ClientsModel::create([
-                "name" => $request->clientName,
-                "package" => $request->selectedPackage,
-            ]);
-    
-            $password = Hash::make($request->password);
-    
-            $admin = UsersModel::create([
-                "user_name" => $request->userName,
-                "first_name" => $request->firstName,
-                "middle_name" => $request->middleName,
-                "last_name" => $request->lastName,
-                "suffix" => $request->suffix,
-                // "birth_date" => $request->selectedPackage,
-    
-                "address" => $request->address,
-                "contact_number" => $request->phoneNumber,
-                "email" => $request->emailAddress,
-                "password" => $password,
-    
-                "user_type" => "Admin",
-                "client_id" => $client->id,
-            ]);
-
-            log::info($client);
-            log::info($admin);
-
-            DB::commit();
+                $client = ClientsModel::create([
+                    "name" => $request->clientName,
+                    "package" => $request->selectedPackage,
+                ]);
         
-            return response()->json([ 'status' => 200 ]);
+                $password = Hash::make($request->password);
+        
+                $admin = UsersModel::create([
+                    "user_name" => $request->userName,
+                    "first_name" => $request->firstName,
+                    "middle_name" => $request->middleName,
+                    "last_name" => $request->lastName,
+                    "suffix" => $request->suffix,
+                    // "birth_date" => $request->selectedPackage,
+        
+                    "address" => $request->address,
+                    "contact_number" => $request->phoneNumber,
+                    "email" => $request->emailAddress,
+                    "password" => $password,
+        
+                    "user_type" => "Admin",
+                    "client_id" => $client->id,
+                ]);
 
-        } catch (\Exception $e) {
-            DB::rollBack();
+                log::info($client);
+                log::info($admin);
 
-            Log::error("Error saving: " . $e->getMessage());
+                DB::commit();
+            
+                return response()->json([ 'status' => 200 ]);
 
-            throw $e;
-        }        
+            } catch (\Exception $e) {
+                DB::rollBack();
+
+                Log::error("Error saving: " . $e->getMessage());
+
+                throw $e;
+            }
+        }    
     }
-
-
 }
