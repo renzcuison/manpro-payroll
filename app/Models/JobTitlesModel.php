1f419c58f46ed07c8676b513d3d0a23aa0b99adb
RedenLamosa <redenlamosa@gmail.com>
2025-01-08 14:46:58 +0800

    Removed Employee Status and Added Job Titles

--- app/Models/JobTitlesModel.php
+++ app/Models/JobTitlesModel.php
@@ -5,16 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class EmployeeStatusModel extends Model
+class JobTitlesModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'employee_status';
+    protected $table = 'job_titles';
 
     protected $primaryKey = 'id';
 
     protected $fillable = [
         'name',
+        'acronym',
         'status',
         'client_id',
     ];
