90c7f910ad0b693307452cecc7a4a360c4c3797c
NocturnalDream <thereclaimer24@gmail.com>
2025-02-14 13:27:46 +0800

    Leave Limit column added to Branch/Department tables

---
+++ database/migrations/2025_02_14_131917_add_leave_limit_to_branches_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddLeaveLimitToBranchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('branches', function (Blueprint $table) {
+            $table->integer('leave_limit')->after('client_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('branches', function (Blueprint $table) {
+            $table->dropColumn('leave_limit');
+        });
+    }
+}
