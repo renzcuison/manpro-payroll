9bd4cfa550e37fe036d088495bb43694f5b80db3
NocturnalDream <thereclaimer24@gmail.com>
2025-02-18 16:37:08 +0800

    Feb 18 updates

--- routes/api.php
+++ routes/api.php
@@ -177,6 +177,9 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
         Route::get('/getApplications', [ApplicationsController::class, 'getApplications']);
 
         Route::get('/getApplicationTypes', [ApplicationsController::class, 'getApplicationTypes']);
+        Route::get('/getFullLeaveDays', [ApplicationsController::class, 'getFullLeaveDays']);
+        Route::get('/getTenureship', [ApplicationsController::class, 'getTenureship']);
+        
         Route::get('/getMyApplications', [ApplicationsController::class, 'getMyApplications']);
         Route::get('/getDashboardApplications', [ApplicationsController::class, 'getDashboardApplications']);
         
@@ -191,9 +194,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
         Route::get('/getLeaveCredits/{id}', [ApplicationsController::class, 'getLeaveCredits']);
         Route::post('/saveLeaveCredits', [ApplicationsController::class, 'saveLeaveCredits']);
         Route::post('/editLeaveCredits', [ApplicationsController::class, 'editLeaveCredits']);
-
-        Route::get('/getFullLeaveDays', [ApplicationsController::class, 'getFullLeaveDays']);
-        Route::get('/getTenureship', [ApplicationsController::class, 'getTenureship']);
     });
 
     Route::prefix('announcements')->group(function () {
