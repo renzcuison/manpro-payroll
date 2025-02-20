436a9d7c838010a46adf792d3f46274de4d9ffab
Jerwin Colipano <jerwincolipano@gmail.com>
2025-02-20 10:23:49 +0800

    Training Tables Migration

---
+++ database/migrations/2025_02_20_090707_create_trainings_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('training_course_id');
+            $table->string('tilte');
+            $table->string('description');
+            $table->string('cover_photo', 128);
+            $table->time('duration');
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('created_by');
+            $table->softDeletes();
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+
+            $table->foreign('training_course_id')->references('id')->on('training_courses');
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->foreign('created_by')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
