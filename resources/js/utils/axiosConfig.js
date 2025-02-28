89ed9aad663bbbc298c40542f1fabcf39879c6da
RedenLamosa <redenlamosa@gmail.com>
2025-02-28 15:29:21 +0800

    Updated Git Ignore

---
+++ resources/js/utils/axiosConfig.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+// LIVE MANPRO - unrbqdtuvg - 2nPGvdYJ6j
+const baseURL = "https://team.manpromanagement.com/api";
+// const baseURL = "https://phplaravel-719501-5238307.cloudwaysapps.com/api"
+
+// STAGING MANPRO - hecfbkmxjy - eusM38kvx3
+// const baseURL = "https://phplaravel-719501-3975047.cloudwaysapps.com/api";
+
+// LOCAL MANPRO
+// const baseURL = "http://localhost:8080/api";
+// const baseURL = "http://192.168.79.2:8080/api";
+// const baseURL = "http://192.168.68.116:8080/api";
+// const baseURL = "http://172.22.24.224:8080/api";
+    
+console.log("Base URL: " + baseURL);
+
+const axiosInstance = axios.create({ baseURL });
+
+export function getJWTHeader(user) {
+    return {
+        Authorization: `Bearer ${user.token}`
+    };
+}
+
+export default axiosInstance; ``
+
+
+        
