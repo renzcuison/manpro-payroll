c9385d0cf40ac4c0657791d2fa0f00e50c9dbd5e
Denver Alibuyog <45626050+denver3542@users.noreply.github.com>
2025-04-03 16:26:58 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,24 +2,14 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'admin/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://team.manpromanagement.com/', 'https://phplaravel-719501-5268927.cloudwaysapps.com/'],
+    'allowed_origins' => [
+        'https://team.manpromanagement.com',
+        'https://phplaravel-719501-5268927.cloudwaysapps.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
