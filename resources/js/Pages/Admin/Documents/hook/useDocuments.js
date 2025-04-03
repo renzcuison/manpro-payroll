ecfecc13f0d2266d4e058687880b3b42b53cac2c
Denver Alibuyog <45626050+denver3542@users.noreply.github.com>
2025-04-03 11:51:24 +0800

    Bug Fix Update

--- resources/js/Pages/Admin/Documents/hook/useDocuments.js
+++ resources/js/Pages/Admin/Documents/hook/useDocuments.js
@@ -2,12 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import axiosInstance, { getJWTHeader } from "../../../../utils/axiosConfig";
 
 async function getDocuments() {
-    const storedUser = localStorage.getItem("nasya_user");
-    const headers = storedUser ? getJWTHeader(JSON.parse(storedUser)) : {};
-    const { data } = await axiosInstance.get("/admin/documents/", {
-        headers,
-    });
-    return data;
+    try {
+        const storedUser = localStorage.getItem("nasya_user");
+        const headers = storedUser ? getJWTHeader(JSON.parse(storedUser)) : {};
+        const { data } = await axiosInstance.get("/admin/documents/", {
+            headers,
+        });
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
 }
 export function useDocuments() {
     const {
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -247,16 +247,3 @@ if (document.getElementById("app")) {
         </QueryClientProvider>
     );
 }
-
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker
-            .register("/service-worker.js")
-            .then((registration) => {
-                console.log("SW registered: ", registration);
-            })
-            .catch((registrationError) => {
-                console.log("SW registration failed: ", registrationError);
-            });
-    });
-}
