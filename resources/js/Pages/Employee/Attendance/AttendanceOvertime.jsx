f31a9ced2e854b54cd8a367635e78ca80534c015
RedenLamosa <redenlamosa@gmail.com>
2025-04-11 18:14:38 +0800

    Attendance Overtime

---
+++ resources/js/Pages/Employee/Attendance/AttendanceOvertime.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import {
+    Table,
+    TableHead,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableRow,
+    Box,
+    Typography,
+    TextField,
+    CircularProgress,
+} from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import dayjs from "dayjs";
+import Layout from "../../../components/Layout/Layout";
+import axiosInstance, { getJWTHeader } from "../../../utils/axiosConfig";
+import { useNavigate } from "react-router-dom";
+
+import AttendanceSummaryDetails from "./Modals/AttendanceSummaryDetails";
+
+const AttendanceSummary = () => {
+    const storedUser = localStorage.getItem("nasya_user");
+    const headers = getJWTHeader(JSON.parse(storedUser));
+    const navigate = useNavigate();
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <Layout title={"AttendanceSummary"}>
+            <Box sx={{ overflowX: "auto", width: "100%", whiteSpace: "nowrap" }} >
+                <Box sx={{ mx: "auto", width: { xs: "100%", md: "1400px" } }}>
+                    <Box sx={{ mt: 5, display: "flex", justifyContent: "space-between", px: 1, alignItems: "center" }} >
+                        <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+                            {" "}Overtime Summary{" "}
+                        </Typography>
+                    </Box>
+
+                    <Box sx={{ mt: 6, p: 3, bgcolor: "#ffffff", borderRadius: "8px" }} >
+                        {isLoading ? (
+                            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: 200 }} >
+                                <CircularProgress />
+                            </Box>
+                        ) : (
+                            <>
+                                <TableContainer>
+                                    <Table aria-label="attendance summary table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell align="center"></TableCell>
+                                                <TableCell align="left">Date</TableCell>
+                                                <TableCell align="center">Overtime In</TableCell>
+                                                <TableCell align="center">Overtime Out</TableCell>
+                                                <TableCell align="center">Total Overtime</TableCell>
+                                                <TableCell align="center">Status</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </>
+                        )}
+                    </Box>
+                </Box>
+            </Box>
+        </Layout >
+    );
+};
+
+export default AttendanceSummary;
