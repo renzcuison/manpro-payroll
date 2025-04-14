9775f547384c8ba8c35aaf6912dd157d510d64af
RedenLamosa <redenlamosa@gmail.com>
2025-04-14 17:14:49 +0800

    Commented Loan Management in Employee Sidebar

--- resources/js/components/Layout/SidebarAdmin.jsx
+++ resources/js/components/Layout/SidebarAdmin.jsx
@@ -273,10 +273,7 @@ const Sidebar = ({ children, closeMini }) => {
 
 
     return (
-        <nav
-            id="sidebar"
-            style={{ zIndex: 1, height: "100vh", overflow: "hidden" }}
-        >
+        <nav id="sidebar" style={{ zIndex: 1, height: "100vh", overflow: "hidden" }} >
             <PerfectScrollbar style={{ height: "100%" }}>
                 <div className="sidebar-content" style={{ height: "100%" }}>
                     <div className="content-header content-header-fullrow px-15">
@@ -289,46 +286,21 @@ const Sidebar = ({ children, closeMini }) => {
                             </span>
                         </div>
                         <div className="content-header-section text-center align-parent sidebar-mini-hidden">
-                            <button
-                                type="button"
-                                className="btn btn-circle btn-dual-secondary d-lg-none align-v-r"
-                                data-toggle="layout"
-                                data-action="sidebar_close"
-                                onClick={closeMini}
-                            >
+                            <button type="button" className="btn btn-circle btn-dual-secondary d-lg-none align-v-r" data-toggle="layout" data-action="sidebar_close" onClick={closeMini} >
                                 <i className="fa fa-times text-danger"></i>
                             </button>
                             <div className="content-header-item">
-                                <img
-                                    src={manpro_logo}
-                                    style={{
-                                        height: "30px",
-                                        marginBottom: "20px",
-                                    }}
-                                />
+                                <img src={manpro_logo} style={{ height: "30px", marginBottom: "20px" }} />
                             </div>
                         </div>
                     </div>
-                    <div
-                        className="content-side content-side-full content-side-user px-10 align-parent"
-                        style={{
-                            backgroundImage:
-                                "linear-gradient(190deg, rgb(42, 128, 15,0.8), rgb(233, 171, 19,1))",
-                        }}
-                    >
+
+                    <div className="content-side content-side-full content-side-user px-10 align-parent" style={{ backgroundImage: "linear-gradient(190deg, rgb(42, 128, 15,0.8), rgb(233, 171, 19,1))" }}>
                         <div className="sidebar-mini-visible-b align-v animated fadeIn">
-                            <img
-                                className="img-avatar img-avatar32"
-                                src={avatar}
-                                alt=""
-                            />
+                            <img className="img-avatar img-avatar32" src={avatar} alt="" />
                         </div>
                         <div className="sidebar-mini-hidden-b text-center">
-                            <Box
-                                display="flex"
-                                flexDirection="column"
-                                alignItems="center"
-                            >
+                            <Box display="flex" flexDirection="column" alignItems="center" >
                                 <Avatar
                                     src={imagePath ? imagePath : HomeLogo}
                                     alt={`${user.first_name} ${user.last_name}`}
@@ -337,16 +309,13 @@ const Sidebar = ({ children, closeMini }) => {
                                         height: 64,
                                         objectFit: "contain",
                                         bgcolor: "grey.300",
-                                        "& .MuiAvatar-img": {
-                                            objectFit: "cover",
-                                        },
+                                        "& .MuiAvatar-img": { objectFit: "cover" },
                                     }}
                                 />
                                 <ul className="list-inline mt-10">
                                     <li className="list-inline-item">
                                         <a className="link-effect text-white font-size-xs font-w600">
-                                            {capitalize(user.first_name)}{" "}
-                                            {capitalize(user.last_name)}
+                                            {capitalize(user.first_name)} {" "} {capitalize(user.last_name)}
                                         </a>
                                     </li>
                                 </ul>
@@ -358,25 +327,12 @@ const Sidebar = ({ children, closeMini }) => {
                             {user.user_type === "Admin" ? (
                                 <>
                                     <li className="nav-main-heading">
-                                        <span
-                                            className="sidebar-mini-hidden"
-                                            style={{ color: "#3d3d3f" }}
-                                        >
-                                            ADMIN
-                                        </span>
+                                        <span className="sidebar-mini-hidden" style={{ color: "#3d3d3f" }} > ADMIN </span>
                                     </li>
 
-                                    <StyledNav
-                                        to={`/dashboard?year=${moment().year()}`}
-                                    >
-                                        <i
-                                            className="si si-grid"
-                                            style={{ color: "#2a800f" }}
-                                        ></i>
-                                        <span
-                                            id="navName"
-                                            className="sidebar-mini-hide"
-                                        >
+                                    <StyledNav to={`/dashboard?year=${moment().year()}`} >
+                                        <i className="si si-grid" style={{ color: "#2a800f" }} ></i>
+                                        <span id="navName" className="sidebar-mini-hide" >
                                             Dashboard
                                         </span>
                                     </StyledNav>
@@ -389,28 +345,19 @@ const Sidebar = ({ children, closeMini }) => {
 
                                     {employeesItems.map((items, index) => {
                                         return (
-                                            <SideItem
-                                                key={index}
-                                                items={items}
-                                            />
+                                            <SideItem key={index} items={items} />
                                         );
                                     })}
 
                                     {attendanceLogs.map((items, index) => {
                                         return (
-                                            <SideItem
-                                                key={index}
-                                                items={items}
-                                            />
+                                            <SideItem key={index} items={items} />
                                         );
                                     })}
 
                                     {payrollItems.map((items, index) => {
                                         return (
-                                            <SideItem
-                                                key={index}
-                                                items={items}
-                                            />
+                                            <SideItem key={index} items={items} />
                                         );
                                     })}
 
@@ -418,52 +365,23 @@ const Sidebar = ({ children, closeMini }) => {
                                         return <SideItem key={index} items={items} />
                                     })}
 
-                                     <StyledNav to={`/admin/loan-management`} >
-                                        <i className="fa fa-credit-card" style={{ color: '#2a800f' }}></i><span id="navName" className="sidebar-mini-hide">Loan Management</span>
-                                    </StyledNav> 
+                                     {/* <StyledNav to={`/admin/loan-management`} > */}
+                                        {/* <i className="fa fa-credit-card" style={{ color: '#2a800f' }}></i><span id="navName" className="sidebar-mini-hide">Loan Management</span> */}
+                                    {/* </StyledNav>  */}
 
                                     <StyledNav to={`/admin/announcements`}>
-                                        <i
-                                            className="fa fa-bullhorn"
-                                            style={{ color: "#2a800f" }}
-                                        ></i>
-                                        <span
-                                            id="navName"
-                                            className="sidebar-mini-hide"
-                                        >
-                                            Announcements
-                                        </span>
+                                        <i className="fa fa-bullhorn" style={{ color: "#2a800f" }} ></i>
+                                        <span id="navName" className="sidebar-mini-hide" > Announcements </span>
                                     </StyledNav>
 
                                     <StyledNav to={`/admin/documents`}>
-                                        <i>
-                                            <DescriptionOutlinedIcon
-                                                sx={{
-                                                    color: palette.success.main,
-                                                }}
-                                            />
-                                        </i>
-                                        <span
-                                            id="navName"
-                                            className="sidebar-mini-hide"
-                                        >
-                                            Documents
-                                        </span>
+                                        <i> <DescriptionOutlinedIcon sx={{ color: palette.success.main }} /> </i>
+                                        <span id="navName" className="sidebar-mini-hide" > Documents </span>
                                     </StyledNav>
 
                                     <StyledNav to={`/admin/trainings`}>
-                                        <i>
-                                            <Iconify
-                                                icon="healthicons:i-training-class-outline"
-                                                style={{ color: "#2a800f" }}
-                                            />
-                                        </i>{" "}
-                                        <span
-                                            id="navName"
-                                            className="sidebar-mini-hide"
-                                        >
-                                            Trainings
-                                        </span>
+                                        <i> <Iconify icon="healthicons:i-training-class-outline" style={{ color: "#2a800f" }} /> </i>{" "}
+                                        <span id="navName" className="sidebar-mini-hide" > Trainings </span>
                                     </StyledNav>
 
                                     {/* <StyledNav to={`/admin/documents`} className={isDocumentsActive || isDocumentEditActive ? 'active' : ''} > */}
@@ -492,26 +410,18 @@ const Sidebar = ({ children, closeMini }) => {
                                 </StyledNav> */}
 
                                     <li className="nav-main-heading">
-                                        <span className="sidebar-mini-hidden text-dark">
-                                            Schedules
-                                        </span>
+                                        <span className="sidebar-mini-hidden text-dark"> Schedules </span>
                                     </li>
 
                                     {workShifts.map((items, index) => {
                                         return (
-                                            <SideItem
-                                                key={index}
-                                                items={items}
-                                            />
+                                            <SideItem key={index} items={items} />
                                         );
                                     })}
 
                                     {workGroups.map((items, index) => {
                                         return (
-                                            <SideItem
-                                                key={index}
-                                                items={items}
-                                            />
+                                            <SideItem key={index} items={items} />
                                         );
                                     })}
 
@@ -523,10 +433,7 @@ const Sidebar = ({ children, closeMini }) => {
 
                                     {settingsItems.map((items, index) => {
                                         return (
-                                            <SideItem
-                                                key={index}
-                                                items={items}
-                                            />
+                                            <SideItem key={index} items={items} />
                                         );
                                     })}
                                 </>
