c8536e7a514c53476ff8993718d2d27f63cb39d2
NocturnalDream <thereclaimer24@gmail.com>
2025-04-14 11:43:07 +0800

    Overtime Applications Table, Model

---
+++ app/Models/ApplicationsOvertimeModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ApplicationsOvertimeModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'applications_overtime';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'user_id',
+        'time_in_id',
+        'time_out_id',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(UsersModel::class, 'user_id');
+    }
+
+    public function timeIn()
+    {
+        return $this->belongsTo(AttendanceLogsModel::class, 'time_in_id');
+    }
+
+    public function timeOut()
+    {
+        return $this->belongsTo(AttendanceLogsModel::class, 'time_out_id');
+    }
+}
