2feae68f45361cd7a866aa8ee274238a93cbf398
smm23 <shilohm.millondaga@gmail.com>
2025-05-21 12:03:51 +0800

    Created tables for Announcements module

---
+++ database/migrations/2025_05_21_114207_create_announcement_employee_status_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('announcement_employee_status', function (Blueprint $table) {
+            $table->id();
+            $table->softDeletes();
+            $table->timestamps();
+            $table->unsignedBigInteger('announcement_id');
+            $table->unsignedBigInteger('user_id');
+            $table->enum('employment_status', ['Active', 'Resigned', 'Terminated']);
+
+            $table->foreign('announcement_id')->references('id')->on('announcements');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('announcement_employee_status');
+    }
+};
