87a760789447dcdf174100ca0e993b2170949533
Denver Alibuyog <45626050+denver3542@users.noreply.github.com>
2025-05-22 17:31:30 +0800

    Update GoogleConnectButton.jsx

--- resources/js/Pages/Admin/Schedules/GoogleConnectButton.jsx
+++ resources/js/Pages/Admin/Schedules/GoogleConnectButton.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const GoogleConnectButton = () => {
     const handleConnect = () => {
         const storedUser = JSON.parse(localStorage.getItem("nasya_user"));
-        window.location.href = `http://127.0.0.1:8000/api/google/redirect?token=${storedUser.token}`;
+        window.location.href = `https://phplaravel-719501-5268927.cloudwaysapps.com/api/google/redirect?token=${storedUser.token}`;
     };
 
     return (
