9545b160a8c813c87ae8b348063d32db669e017a
denver3542 <45626050+denver3542@users.noreply.github.com>
2025-05-23 11:09:53 +0800

    Update axiosConfig.js

--- resources/js/utils/axiosConfig.js
+++ resources/js/utils/axiosConfig.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // LIVE MANPRO - unrbqdtuvg - cknevjydbm
-// const baseURL = "https://team.manpromanagement.com/api";
+const baseURL = "https://team.manpromanagement.com/api";
 
 // STAGING MANPRO - anxrqwcazv
-const baseURL = "https://phplaravel-719501-5268927.cloudwaysapps.com/api";
+// const baseURL = "https://phplaravel-719501-5268927.cloudwaysapps.com/api";
 
 // LOCAL MANPRO
 // const baseURL = "http://localhost:8000/api";
@@ -13,7 +13,7 @@ console.log("Base URL: " + baseURL);
 
 const axiosInstance = axios.create({ baseURL });
 
-export function getJWTHeader(user) {    
+export function getJWTHeader(user) {
     return {
         Authorization: `Bearer ${user.token}`,
     };
