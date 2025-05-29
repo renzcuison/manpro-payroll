3518fe793c009aee7cd76c1064429f19036bc8b0
Denver Alibuyog <45626050+denver3542@users.noreply.github.com>
2025-05-29 16:25:12 +0800

    Update useMilestones.js

--- resources/js/Pages/Admin/Milestones/hook/useMilestones.js
+++ resources/js/Pages/Admin/Milestones/hook/useMilestones.js
@@ -1,18 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import axiosInstance, { getJWTHeader } from "../../../../utils/axiosConfig";
 
+async function getMilestones() {
+    const storedUser = localStorage.getItem("nasya_user");
+    const headers = storedUser ? getJWTHeader(JSON.parse(storedUser)) : {};
+    const { data } = await axiosInstance.get("/admin/milestones/", {
+        headers,
+    });
+    return data;
+}
+
 export function useMilestones() {
     const { data, isLoading, isFetched, isFetching, refetch, isError } =
-        useQuery(["milestones"], async () => {
-            const storedUser = localStorage.getItem("nasya_user");
-            const headers = storedUser
-                ? getJWTHeader(JSON.parse(storedUser))
-                : {};
-            const { data } = await axiosInstance.get("/admin/milestones/", {
-                headers,
-            });
-            return data;
-        });
+        useQuery(["milestones"], () => getMilestones());
 
     return { data, isLoading, isFetching, isFetched, refetch, isError };
 }
