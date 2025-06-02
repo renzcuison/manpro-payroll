86ab7d42991e4256f3bf1784098f9a460464dda6
crasher672 <crasher672@gmail.com>
2025-06-02 15:35:31 +0800

    LeaveCreditUpdated

--- routes/api.php
+++ routes/api.php
@@ -147,6 +147,10 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
         Route::get('/getEmployeeLeaveCredits', [EmployeesController::class, 'getEmployeeLeaveCredits']);
 
+        //test v
+        Route::get('/getLeaveCreditByUser/{username}', [EmployeesController::class,'getLeaveCreditByUser']);
+
+
         Route::get('/getMyAvatar', [EmployeesController::class, 'getMyAvatar']);
         Route::get('/getMyDetails', [EmployeesController::class, 'getMyDetails']);
         Route::get('/getEmployeeDetails', [EmployeesController::class, 'getEmployeeDetails']);
