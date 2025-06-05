63d9523aa7bfd6534d4817199976812ed8f11d5f
smm23 <shilohm.millondaga@gmail.com>
2025-06-05 10:44:57 +0800

    fix: restore axiosConfig.js

--- resources/js/utils/axiosConfig.js
+++ resources/js/utils/axiosConfig.js
@@ -1,14 +1,6 @@
 import axios from "axios";
 
-// LIVE MANPRO - unrbqdtuvg - cknevjydbm
-// const baseURL = "https://team.manpromanagement.com/api";
-
-// STAGING MANPRO - anxrqwcazv
-// const baseURL = "https://phplaravel-719501-5268927.cloudwaysapps.com/api";
-
-// LOCAL MANPRO
- const baseURL = "http://localhost:8000/api";
-// const baseURL = import.meta.env.VITE_API_URL || "https://team.manpromanagement.com/api";
+const baseURL = import.meta.env.VITE_API_URL || "https://team.manpromanagement.com/api";
 
 console.log("Base URL: " + baseURL);
 
