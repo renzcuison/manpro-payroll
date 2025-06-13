bd966eb0714fe14ee89f6f24b074b648f31c059e
ShilohMillondaga <shilohm.millondaga@gmail.com>
2025-06-13 16:53:25 +0800

    refactor: create model for salary plans

---
+++ app/Models/SalaryPlansModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SalaryPlansModel extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'salary_plans';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'salary_grade',
+        'amount',
+        'client_id',
+    ];
+}
