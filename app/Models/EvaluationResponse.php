4f4f1a1ffe4d48cf2cd394e49f2a4ac6d89b43c0
ShilohMillondaga <shilohm.millondaga@gmail.com>
2025-06-13 14:52:22 +0800

    refactor: add signatures to evaluation response model

--- app/Models/EvaluationResponse.php
+++ app/Models/EvaluationResponse.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 class EvaluationResponse extends Model
 {
@@ -62,5 +64,10 @@ class EvaluationResponse extends Model
     public function textAnswers()
     {
         return $this->hasMany(EvaluationTextAnswer::class, 'response_id');
+    }
+
+        public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('signatures');
     }
 }
