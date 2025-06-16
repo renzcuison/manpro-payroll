af3be4b8847b93013c714e7c22d71cc8e00c70f8
SyBorg00 <agawinsylvannjules@gmail.com>
2025-06-16 09:43:45 +0800

    minor updates on departments again

--- resources/js/hooks/useDepartments.js
+++ resources/js/hooks/useDepartments.js
@@ -6,14 +6,22 @@ const headers = storedUser ? getJWTHeader(JSON.parse(storedUser)) : [];
 
 export function useDepartments(departmentId = null){
     const departments = useQuery(["departments"], async () => {
-        const {data} = await axiosInstance.get("settings/getDepartments", {
-            headers,
-        });
+        const {data} = await axiosInstance.get("settings/getDepartments", { headers });
+        return data;
+    });
+
+    const departmentsWithPositions = useQuery(['departmentsWithPositions'], async () => {
+        const {data} = await axiosInstance.get('/settings/getDepartmentWithEmployeePosition', { headers });
+        return data;
+    });
+
+    const departmentPositions = useQuery(['departmentPositions'], async () => {
+        const {data} = await axiosInstance.get('/settings/getDepartmentPositions', { headers });
         return data;
     });
 
     return{
-        departments
+        departments, departmentPositions, departmentsWithPositions
     }
 
 }
