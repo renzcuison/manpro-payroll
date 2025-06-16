7eebcfd56fe53bffffaabb6e8e39e20145c90158
ShilohMillondaga <shilohm.millondaga@gmail.com>
2025-06-16 11:41:24 +0800

    feat: add salary plans FE and BE

---
+++ app/Http/Controllers/SalaryPlansController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SalaryPlansModel;
+use App\Models\UsersModel;
+use App\Models\ClientsModel;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+
+use Carbon\Carbon;
+
+class SalaryPlansController extends Controller {
+    public function checkUser(){
+        if (Auth::check()) {
+            $user = Auth::user();
+            if ($user->user_type == 'Admin') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getSalaryPlans(Request $request){
+        $user = Auth::user();
+
+        if ($this->checkUser()) {
+            $salaryPlans = SalaryPlansModel::where('client_id', $user->client_id)
+                ->where('deleted_at', NULL)
+                ->orderByRaw('CAST(salary_grade AS UNSIGNED) ASC') // <-- Add this line
+                ->get();
+
+            return response()->json(['status' => 200, 'salaryPlans' => $salaryPlans]);
+        } 
+
+        return response()->json(['status' => 403, 'message' => 'Unauthorized'], 403);
+    }
+
+    public function saveSalaryGrade(Request $request){
+        $validated = $request->validate([
+            'salary_grade' => 'required',
+            'amount' => 'required',
+        ]);
+
+        if ($this->checkUser() && $validated) {
+            $user = Auth::user();
+            $client = ClientsModel::find($user->client_id);
+
+            try {
+                DB::beginTransaction();
+
+                $salaryPlan = SalaryPlansModel::create([
+                    "salary_grade" => $request->salary_grade,
+                    "amount" => $request->amount,
+                    "client_id" => $client->id,
+                ]);
+
+                DB::commit();
+
+                return response()->json(['status' => 200, 'salaryPlan' => $salaryPlan]);
+            } catch (\Exception $e) {
+                DB::rollBack();
+
+                Log::error("Error saving: " . $e->getMessage());
+
+                throw $e;
+            }
+        } 
+
+        return response()->json(['status' => 403, 'message' => 'Unauthorized'], 403);
+    }
+
+    public function editSalaryGrade(Request $request, $id){
+        $validated = $request->validate([
+            'salary_grade' => 'required',
+            'amount' => 'required',
+        ]);
+
+        if ($this->checkUser() && $validated) {
+            $user = Auth::user();
+            $salaryGrade = SalaryPlansModel::find($request->input('id'));
+
+            $salaryGrade->salary_grade = $request->input('salary_grade');
+            $salaryGrade->amount = $request->input('amount');
+
+            $salaryGrade->save();
+
+            return response()->json(['status' => 200]);
+        } 
+
+        return response()->json(['status' => 403, 'message' => 'Unauthorized'], 403);
+    }
+
+    public function deleteSalaryGrade(Request $request){
+        $salary_grade = SalaryPlansModel::find($request->salary_grade);
+
+        if ($salary_grade) {
+            $salary_grade->delete();
+            return response()->json(['message' => 'Salary grade soft deleted successfully.']);
+        } else {
+            return response()->json(['message' => 'Salary grade not found.'], 404);
+        }
+    }
+}
+
---
+++ resources/js/Pages/Admin/SalaryPlans/EmployeeSalaryPlansList.jsx
@@ -0,0 +1,187 @@
+import React, { useEffect, useState } from 'react';
+import { Table, TableHead, TableBody, TableCell, TableContainer, TableRow, Box, Typography, Grid, TextField, FormControl, CircularProgress, Button, IconButton} from '@mui/material';
+import Layout from '../../../components/Layout/Layout';
+import axiosInstance, { getJWTHeader } from '../../../utils/axiosConfig';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Swal from "sweetalert2";
+import { Link } from 'react-router-dom';
+
+import SalaryGradeAdd from './Modals/SalaryGradeAdd';
+import SalaryGradeEdit from './Modals/SalaryGradeEdit';
+
+const SalaryPlans = () => {
+    const storedUser = localStorage.getItem("nasya_user");
+    const headers = getJWTHeader(JSON.parse(storedUser));
+    
+    const [isLoading, setIsLoading] = useState(true);
+    const [salaryPlans, setSalaryPlans] = useState([]);
+    const [loadSalaryGrade, setLoadSalaryGrade] = useState(null);
+    const [openAddSalaryGrade, setOpenAddSalaryGrade] = useState(false);
+    const [openEditSalaryGrade, setOpenEditSalaryGrade] = useState(false);
+
+    useEffect(() => {
+        fetchSalaryPlans();
+    }, []);
+
+    const fetchSalaryPlans = () => {
+        axiosInstance
+            .get("/getSalaryPlans", { headers })
+            .then((response) => {
+                setSalaryPlans(response.data.salaryPlans || []); // Ensure it's an array
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.error("Error fetching application types:", error);
+                setIsLoading(false);
+            });
+    };
+
+    //Add Salary Plan Functions
+    const handleOpenAddSalaryGrade = () => {
+        setOpenAddSalaryGrade(true);
+    }
+
+    const handleCloseAddSalaryGrade = () => {
+        setOpenAddSalaryGrade(false);
+        fetchSalaryPlans();
+    }
+
+    // Edit Salary Plan Functions
+    const handleOpenEditSalaryGrade = (salaryGrade) => {
+        setLoadSalaryGrade(salaryGrade)
+        setOpenEditSalaryGrade(true);
+    }
+
+    const handleCloseEditSalaryGrade = () => {
+        setOpenEditSalaryGrade(false);
+        fetchSalaryPlans();
+    }
+
+    const handleDeleteSalaryGrade = (salaryGrade) => {
+            Swal.fire({
+                customClass: { container: "my-swal" },
+                title: `Delete Salary Grade?`,
+                text: `Are you sure you want to delete this salary grade?\This action can't be undone!`,
+                icon: "warning",
+                showConfirmButton: true,
+                confirmButtonText: "Delete",
+                confirmButtonColor: "#E9AE20",
+                showCancelButton: true,
+                cancelButtonText: "Cancel",
+            }).then((res) => {
+                if (res.isConfirmed) {
+    
+                    const data = {
+                        salary_grade: salaryGrade.id,
+                    };
+
+                    axiosInstance.post('/deleteSalaryGrade', data, { headers })
+                        .then(response => {
+                            Swal.fire({
+                                customClass: { container: 'my-swal' },
+                                text: "Salary grade deleted successfully!",
+                                icon: "success",
+                                timer: 1000,
+                                showConfirmButton: true,
+                                confirmButtonText: 'Proceed',
+                                confirmButtonColor: '#177604',
+                            }).then(() => {
+                                fetchSalaryPlans();
+                            });
+                        })
+                        .catch(error => {
+                            console.error('Error:', error);
+                        });
+    
+                }
+            });
+        };
+
+    return (
+        <Layout title={"Salary Plans"}>
+            <Box sx={{ overflowX: 'auto', width: '100%', whiteSpace: 'nowrap' }}>
+                <Box sx={{ mx: 'auto', width: { xs: '100%', md: '1400px' } }}>
+                    <Box sx={{ mt: 5, display: 'flex', justifyContent: 'space-between', px: 1, alignItems: 'center' }}>
+                        <Typography variant="h4" sx={{ fontWeight: 'bold', display: 'flex', alignItems: 'center' }}>
+                            &nbsp; Salary Plans
+                        </Typography>
+
+                        <Button variant="contained" color="primary" onClick={handleOpenAddSalaryGrade}>
+                            <p className='m-0'><i className="fa fa-plus"></i> Add </p>
+                        </Button>
+                    </Box>
+
+                    <Box sx={{ mt: 6, p: 3, bgcolor: '#ffffff', borderRadius: '8px' }}>
+                        {isLoading ? (
+                            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: 400 }}>
+                                <CircularProgress />
+                            </Box>
+                        ) : (
+                            <>
+                                <TableContainer style={{ overflowX: 'auto' }} sx={{ minHeight: 400 }}>
+                                    <Table aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell sx={{ fontWeight: 'bold', fontSize: 16, width: "20%" }} align="center"> Salary Grade </TableCell>
+                                                <TableCell sx={{ fontWeight: 'bold', fontSize: 16, width: "60%" }} align="left"> Amount </TableCell>
+                                                <TableCell sx={{ fontWeight: 'bold', fontSize: 16, width: "20%" }} align="center"> Actions </TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {salaryPlans.length > 0 ? (
+                                                salaryPlans.map((salaryPlan) => (
+                                                    // onClick={() => setOpenEditSalaryType(salaryPlan)}
+                                                    <TableRow key={salaryPlan.id}>
+                                                        <TableCell sx={{fontSize: 14}} align="center">{salaryPlan.salary_grade}</TableCell>
+                                                        <TableCell sx={{ fontSize: 14 }} align="left">
+                                                            ₱ {Number(salaryPlan.amount).toLocaleString('en-PH', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                                                        </TableCell>
+                                                        <TableCell sx={{fontSize: 14}} align="center">
+                                                            <IconButton
+                                                                color="primary"
+                                                                onClick={() => handleOpenEditSalaryGrade(salaryPlan)}
+                                                                aria-label="edit"
+                                                                size="small"
+                                                            >
+                                                                <EditIcon />
+                                                            </IconButton>
+                                                            <IconButton
+                                                                color="error"
+                                                                onClick={(event) => {
+                                                                    event.stopPropagation();
+                                                                    handleDeleteSalaryGrade(salaryPlan);
+                                                                }}
+                                                                aria-label="delete"
+                                                                size="small"
+                                                            >
+                                                                <DeleteIcon />
+                                                            </IconButton>
+                                                        </TableCell>
+                                                    </TableRow>
+                                                ))
+                                            ) : (
+                                                <TableRow>
+                                                    <TableCell colSpan={3} align="center"> No Salary Plans </TableCell>
+                                                </TableRow>
+                                            )}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </>
+                        )}
+                    </Box>
+                </Box>
+                {openAddSalaryGrade &&
+                    <SalaryGradeAdd open={openAddSalaryGrade} close={handleCloseAddSalaryGrade} />
+                }
+                {openEditSalaryGrade &&
+                    <SalaryGradeEdit open={openEditSalaryGrade} close={handleCloseEditSalaryGrade} salaryGradeInfo={loadSalaryGrade} />
+                }
+            </Box>
+        </Layout>
+    );
+
+
+}
+export default SalaryPlans;
