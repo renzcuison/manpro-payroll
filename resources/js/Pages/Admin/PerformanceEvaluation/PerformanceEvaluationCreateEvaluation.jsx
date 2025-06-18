b4efb2db0eb7ec0ae8f850cda57fdc22a9c0af76
KristyleModin <155639837+KristyleModin@users.noreply.github.com>
2025-06-18 14:53:46 +0800

    Change the Save, Saved to Send, Sent

--- resources/js/Pages/Admin/PerformanceEvaluation/PerformanceEvaluationCreateEvaluation.jsx
+++ resources/js/Pages/Admin/PerformanceEvaluation/PerformanceEvaluationCreateEvaluation.jsx
@@ -113,8 +113,8 @@ const PerformanceEvaluationCreateEvaluation = () => {
             await axiosInstance.post('/saveEvaluationResponse', payload, { headers });
             Swal.fire({
                 icon: 'success',
-                title: 'Evaluation Response Saved!',
-                text: 'The evaluation response has been successfully saved.',
+                title: 'Evaluation Response Sent!',
+                text: 'The evaluation response has been successfully sent.',
                 confirmButtonText: 'OK',
             }).then(() => {
                 navigate(-1);
@@ -572,7 +572,7 @@ const PerformanceEvaluationCreateEvaluation = () => {
                             sx={{ padding: '10px 20px' }}
                             startIcon={<SaveIcon />}
                         >
-                            Save Evaluation
+                            Sent Evaluation
                         </Button>
                     </Box>
                 </form>
