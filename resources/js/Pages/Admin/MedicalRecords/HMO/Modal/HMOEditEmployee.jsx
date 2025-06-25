035e334da80480769bc976d0a0f0c938719137dd
adcibanez <adcibanez@addu.edu.ph>
2025-06-25 10:11:42 +0800

    Update HMOEditEmployee.jsx

--- resources/js/Pages/Admin/MedicalRecords/HMO/Modal/HMOEditEmployee.jsx
+++ resources/js/Pages/Admin/MedicalRecords/HMO/Modal/HMOEditEmployee.jsx
@@ -153,15 +153,23 @@ const HMOEditEmployee = ({ open, close, employeePlanId, refreshEmployees }) => {
             headers: { Authorization: `Bearer ${user.token}` },
         })
         .then((res) => {
-            Swal.fire({
-                icon: 'success',
-                title: "Success",
-                text: 'HMO Employee deleted successfully.',
-                timer: 2000,
-                showConfirmButton: false
-            });
-            close();
-            refreshEmployees();
+            if (res.data.status === 200) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: "Success",
+                        text: 'HMO Employee deleted successfully.',
+                        timer: 2000,
+                        showConfirmButton: false
+                    });
+                    close();
+                    refreshEmployees();
+                } else if (res.data.status === 400) {
+                    Swal.fire({
+                        icon: 'info',
+                        title: 'Cannot Delete',
+                        text: 'This employee has dependents assigned and cannot be deleted.',
+                    });
+                }
         })
         .catch((err) => {
             console.error(err);
