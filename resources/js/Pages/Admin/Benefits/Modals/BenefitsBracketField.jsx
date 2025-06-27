e89b73cf67e734a0c9c2c5c1a48b6bc24a79c3a6
SyBorg00 <agawinsylvannjules@gmail.com>
2025-06-27 13:02:57 +0800

    updates

--- resources/js/Pages/Admin/Benefits/Modals/BenefitsBracketField.jsx
+++ resources/js/Pages/Admin/Benefits/Modals/BenefitsBracketField.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Button,  TextField, Typography , InputAdornment, TableContainer, Table,
-TableHead, TableBody, TableRow, TableCell } from '@mui/material';
+TableHead, TableBody, TableRow, TableCell, Tooltip } from '@mui/material';
 import { CgAdd, CgTrash } from "react-icons/cg";  
 
 const BenefitsBracketField = ({type, bracketsList, onAdd, onChange, onRemove, bracketListErrors}) => {
@@ -25,24 +25,39 @@ const BenefitsBracketField = ({type, bracketsList, onAdd, onChange, onRemove, br
                         {bracketsList.length > 0 && bracketsList.map((item, index) => (
                             <TableRow key={index}>
                                 <TableCell sx={{ borderRight: '1px solid #e0e0e0' }}>
-                                    <TextField
-                                        size="small"
-                                        variant="outlined"
-                                        error={bracketListErrors[index]?.range_start}
-                                        InputProps={{
-                                            startAdornment: <InputAdornment position="start">₱</InputAdornment>,
-                                            readOnly: true,
-                                            
-                                        }}
-                                        value={item.range_start}
-                                        onChange={(e) => onChange(index, 'range_start', e.target.value)}
-                                    />
+                                    <Tooltip title="Automatic">
+                                        <TextField
+                                            size="small"
+                                            variant="outlined"
+                                            error={bracketListErrors[index]?.range_start}
+                                            sx={{
+                                                '& label.Mui-focused': {color: "#97a5ba"},
+                                                '& .MuiOutlinedInput-root': {
+                                                    '&.Mui-focused fieldset': {borderColor: "#97a5ba"},
+                                                },
+                                            }}
+                                            InputProps={{
+                                                startAdornment: <InputAdornment position="start">₱</InputAdornment>,
+                                                readOnly: true,
+                                        
+                                            }}
+                                        
+                                            value={item.range_start}
+                                            onChange={(e) => onChange(index, 'range_start', e.target.value)}
+                                        />
+                                    </Tooltip>
                                 </TableCell>
                                 <TableCell sx={{ borderRight: '1px solid #e0e0e0' }}>
                                     <TextField
                                         size="small"
                                         variant="outlined"
                                         error={bracketListErrors[index]?.range_end}
+                                        sx={{
+                                            '& label.Mui-focused': {color: index === bracketsList.length -1 ? "#97a5ba": null },
+                                            '& .MuiOutlinedInput-root': {
+                                                '&.Mui-focused fieldset': {borderColor: index === bracketsList.length -1 ? "#97a5ba": null},
+                                            },
+                                        }}
                                         placeholder={index === bracketsList.length - 1 ? 'and above' : ''}
                                         InputProps={{
                                             startAdornment: <InputAdornment position="start">₱</InputAdornment>,
