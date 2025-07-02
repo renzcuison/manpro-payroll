22ba17396e7a20d777a4a87562c0c7eb26bafbba
RedenLamosa <redenlamosa@gmail.com>
2025-07-02 16:16:27 +0800

    Import Employee Function

--- resources/js/Pages/Admin/Employees/Modals/ImportVerification.jsx
+++ resources/js/Pages/Admin/Employees/Modals/ImportVerification.jsx
@@ -6,7 +6,7 @@ import InputAdornment from '@mui/material/InputAdornment';
 import React, { useState, useEffect } from 'react';
 import axiosInstance, { getJWTHeader } from '../../../../utils/axiosConfig';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import Swal from 'sweetalert2';
@@ -17,6 +17,7 @@ import 'react-quill/dist/quill.snow.css';
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import localizedFormat from "dayjs/plugin/localizedFormat";
+
 dayjs.extend(utc);
 dayjs.extend(localizedFormat);
 
@@ -27,77 +28,111 @@ const ImportVerification = ({ open, close, data }) => {
 
     const [firstNameError, setFirstNameError] = useState(false);
     const [lastNameError, setLastNameError] = useState(false);
+    const [userNameError, setUserNameError] = useState(false);
     const [emailError, setEmailError] = useState(false);
     const [birthdateError, setBirthdateError] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
+    const [confirmError, setConfirmError] = useState(false);
 
-    const [firstName, setFirstName] = useState();
-    const [middleName, setMiddleName] = useState();
-    const [lastName, setLastName] = useState();
-    const [suffix, setSuffix] = useState();
+    const [firstName, setFirstName] = useState('');
+    const [middleName, setMiddleName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [suffix, setSuffix] = useState('');
+    const [userName, setUserName] = useState('');
     const [email, setEmail] = useState();
-    const [birthdate, setBirthdate] = useState();
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [address, setAddress] = useState('');
+    const [birthdate, setBirthdate] = useState(null);
 
-    const [password, setPassword] = useState();
+    const [password, setPassword] = useState('');
+    const [confirm, setConfirm] = useState('');
 
     useEffect(() => {
         if (data) {
             const fName = data["First Name"] || "";
+            const mname = data["Middle Name"] || "";
             const lName = data["Last Name"] || "";
             const bDate = data["Birthdate"] || "";
 
             setFirstName(fName);
-            setMiddleName(data["Middle Name"] || "");
+            setMiddleName(mname);
             setLastName(lName);
             setSuffix(data["Suffix"] || "");
             setEmail(data["Email"] || "");
-            setBirthdate(bDate);
+            setUserName(data["Email"] || "");
+
+            const formattedDate = dayjs(bDate, ["MM/DD/YYYY", "M/D/YYYY", "YYYY-MM-DD"]);
+            setBirthdate(formattedDate.isValid() ? formattedDate : null);
 
             // Generate password
             const firstPart = fName ? fName.charAt(0).toUpperCase() + fName.slice(1, 3) : "";
             const lastPart = lName ? lName.charAt(0).toUpperCase() + lName.slice(1, 3) : "";
             const generatedPassword = `${firstPart}${lastPart}@${bDate}`;
+
             setPassword(generatedPassword);
+            setConfirm(generatedPassword);
         }
     }, [data]);
 
-        const checkInput = (event) => {
-            event.preventDefault();
-    
-            if (!firstName) {
-                setFirstNameError(true);
-            } else {
-                setFirstNameError(false);
-            }
-    
-            if (!lastName) {
-                setLastNameError(true);
-            } else {
-                setLastNameError(false);
-            }
-    
-            if (!email) {
-                setEmailError(true);
-            } else {
-                setEmailError(false);
-            }
-    
-            if (!birthdate) {
-                setBirthdateError(true);
-            } else {
-                setBirthdateError(false);
-            }
-    
-            if (!password) {
-                setPasswordError(true);
-            } else {
-                setPasswordError(false);
-            }
-    
-            if (!firstName || !lastName || !email || !birthdate || !password ) {
+    const checkInput = (event) => {
+        event.preventDefault();
+
+        if (!firstName) {
+            setFirstNameError(true);
+        } else {
+            setFirstNameError(false);
+        }
+
+        if (!lastName) {
+            setLastNameError(true);
+        } else {
+            setLastNameError(false);
+        }
+
+        if (!email) {
+            setEmailError(true);
+        } else {
+            setEmailError(false);
+        }
+
+        if (!userName) {
+            setUserNameError(true);
+        } else {
+            setUserNameError(false);
+        }
+
+        if (!birthdate) {
+            setBirthdateError(true);
+        } else {
+            setBirthdateError(false);
+        }
+
+        if (!password) {
+            setPasswordError(true);
+        } else {
+            setPasswordError(false);
+        }
+
+        if (!confirm) {
+            setConfirmError(true);
+        } else {
+            setConfirmError(false);
+        }
+
+        if (!firstName || !lastName || !userName || !email || !birthdate || !password || !confirm) {
+            Swal.fire({
+                customClass: { container: 'my-swal' },
+                text: "All fields must be filled!",
+                icon: "error",
+                showConfirmButton: true,
+                confirmButtonColor: '#177604',
+            });
+        } else {
+            if (confirm != password) {
+                setConfirmError(true);
                 Swal.fire({
                     customClass: { container: 'my-swal' },
-                    text: "All fields must be filled!",
+                    text: "Password does not match!",
                     icon: "error",
                     showConfirmButton: true,
                     confirmButtonColor: '#177604',
@@ -119,50 +154,51 @@ const ImportVerification = ({ open, close, data }) => {
                     }
                 });
             }
+        }
+    };
+
+    const saveInput = (event) => {
+        event.preventDefault();
+
+        const data = {
+            firstName: firstName,
+            middleName: middleName,
+            lastName: lastName,
+            suffix: suffix,
+            userName: email,
+            emailAddress: email,
+            birthdate: birthdate,
+            phoneNumber: "",
+            address: "",
+            password: password,
         };
 
-        const saveInput = (event) => {
-            event.preventDefault();
-    
-            const data = {
-                firstName: firstName,
-                middleName: middleName,
-                lastName: lastName,
-                suffix: suffix,
-                userName: email,
-                emailAddress: email,
-                birthdate: birthdate,
-                phoneNumber: "",
-                address: "",
-                password: password,
-            };
-    
-            axiosInstance.post('/employee/saveEmployee', data, { headers })
-                .then(response => {
-                    if (response.data.status === 200) {
+        axiosInstance.post('/employee/saveEmployee', data, { headers })
+            .then(response => {
+                if (response.data.status === 200) {
+                    Swal.fire({
+                        customClass: { container: 'my-swal' },
+                        text: "Employee saved successfully!",
+                        icon: "success",
+                        timer: 1000,
+                        showConfirmButton: true,
+                        confirmButtonText: 'Proceed',
+                        confirmButtonColor: '#177604',
+                    }).then(() => {
                         Swal.fire({
                             customClass: { container: 'my-swal' },
                             text: "Employee saved successfully!",
                             icon: "success",
-                            timer: 1000,
                             showConfirmButton: true,
-                            confirmButtonText: 'Proceed',
                             confirmButtonColor: '#177604',
-                        }).then(() => {
-                            Swal.fire({
-                                customClass: { container: 'my-swal' },
-                                text: "Employee saved successfully!",
-                                icon: "success",
-                                showConfirmButton: true,
-                                confirmButtonColor: '#177604',
-                            });
                         });
-                    }
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-        };
+                    });
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    };
 
     return (
         <>
@@ -235,9 +271,28 @@ const ImportVerification = ({ open, close, data }) => {
                             </FormControl>
                         </FormGroup>
 
-                        <FormGroup row={true} className="d-flex justify-content-between" sx={{ '& label.Mui-focused': {color: '#97a5ba'}, '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': {borderColor: '#97a5ba'}}, }}>
-                            <FormControl sx={{ marginBottom: 3, width: '32%', '& label.Mui-focused': { color: '#97a5ba' },
-                                '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' }},
+                        <FormGroup row={true} className="d-flex justify-content-between" sx={{
+                            '& label.Mui-focused': { color: '#97a5ba' },
+                            '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' } },
+                        }}>
+                            <FormControl sx={{
+                                marginBottom: 3, width: '32%', '& label.Mui-focused': { color: '#97a5ba' },
+                                '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' } },
+                            }}>
+                                <TextField
+                                    required
+                                    id="userName"
+                                    label="User Name"
+                                    variant="outlined"
+                                    value={userName}
+                                    error={userNameError}
+                                    onChange={(e) => setUserName(e.target.value)}
+                                />
+                            </FormControl>
+
+                            <FormControl sx={{
+                                marginBottom: 3, width: '32%', '& label.Mui-focused': { color: '#97a5ba' },
+                                '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' } },
                             }}>
                                 <TextField
                                     required
@@ -250,31 +305,88 @@ const ImportVerification = ({ open, close, data }) => {
                                 />
                             </FormControl>
 
-                            <FormControl sx={{ marginBottom: 3, width: '32%', '& label.Mui-focused': { color: '#97a5ba' },
-                                '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' }},
+                            <FormControl sx={{
+                                marginBottom: 3, width: '32%', '& label.Mui-focused': { color: '#97a5ba' },
+                                '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' } },
                             }}>
                                 <TextField
-                                    required
-                                    id="birthDate"
-                                    label="Birth Date"
+                                    id="phoneNumber"
+                                    label="Phone Number"
                                     variant="outlined"
-                                    value={birthdate ?? ''}
-                                    error={birthdateError}
-                                    onChange={(e) => setBirthdate(e.target.value)}
+                                    value={phoneNumber}
+                                    onChange={(e) => setPhoneNumber(e.target.value)}
+                                />
+                            </FormControl>
+                        </FormGroup>
+
+                        <FormGroup row={true} className="d-flex justify-content-between" sx={{
+                            '& label.Mui-focused': { color: '#97a5ba' },
+                            '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' } },
+                        }}>
+                            <FormControl sx={{
+                                marginBottom: 3, width: '70%', '& label.Mui-focused': { color: '#97a5ba' },
+                                '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' } },
+                            }}>
+                                <TextField
+                                    id="address"
+                                    label="Address"
+                                    variant="outlined"
+                                    value={address}
+                                    onChange={(e) => setAddress(e.target.value)}
                                 />
                             </FormControl>
 
-                            <FormControl sx={{ marginBottom: 3, width: '32%', '& label.Mui-focused': { color: '#97a5ba' },
-                                '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' }},
+                            <FormControl sx={{
+                                marginBottom: 3, width: '28%', '& label.Mui-focused': { color: '#97a5ba' },
+                                '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' } },
+                            }}>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DatePicker
+                                        id="birthdate"
+                                        label="Birth Date"
+                                        value={birthdate}
+                                        onChange={(newValue) => setBirthdate(newValue)}
+                                        slotProps={{
+                                            textField: { required: true, error: birthdateError }
+                                        }}
+                                    />
+                                </LocalizationProvider>
+                            </FormControl>
+                        </FormGroup>
+
+                        <FormGroup row={true} className="d-flex justify-content-between" sx={{
+                            '& label.Mui-focused': { color: '#97a5ba' },
+                            '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' } },
+                        }}>
+                            <FormControl sx={{
+                                marginBottom: 3, width: '49%', '& label.Mui-focused': { color: '#97a5ba' },
+                                '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' } },
                             }}>
                                 <TextField
                                     required
-                                    id="password"
+                                    id="Password"
                                     label="Password"
                                     variant="outlined"
-                                    value={password ?? ''}
+                                    type="password"
+                                    value={password}
                                     error={passwordError}
                                     onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </FormControl>
+
+                            <FormControl sx={{
+                                marginBottom: 3, width: '49%', '& label.Mui-focused': { color: '#97a5ba' },
+                                '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#97a5ba' } },
+                            }}>
+                                <TextField
+                                    required
+                                    id="confirmPassword"
+                                    label="Confirm Password"
+                                    variant="outlined"
+                                    type="password"
+                                    value={confirm}
+                                    error={confirmError}
+                                    onChange={(e) => setConfirm(e.target.value)}
                                 />
                             </FormControl>
                         </FormGroup>
